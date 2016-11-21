1b28df957f704f261ed09b5b7efe791cbbf8f4b9
shekodn <sdiazn19@gmail.com>
2016-11-20 23:03:18 -0600

    adds password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+
+    before_save { self.email = email.downcase }
+    validates :fname,  presence: true, length: { maximum: 50 }
+    validates :lname,  presence: true, length: { maximum: 50 }
+    validates :phone,  presence: true, length: { maximum: 15 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121030809) do
+ActiveRecord::Schema.define(version: 20161121045912) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -73,8 +73,10 @@ ActiveRecord::Schema.define(version: 20161121030809) do
     t.string   "username"
     t.string   "email"
     t.string   "phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "vacancies", force: :cascade do |t|
